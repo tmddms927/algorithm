4e677d7f732b265a8309d2b655d1f726924891cf
tmddms927 <tmddms927@gmail.com>
2021-05-11 20:19:19 -0700

    feat: add 2438.c

---
+++ easy/2438.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i, j;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 0; j <i; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
