5969994283f8b28782931e7a731b6a4fcf38dcf7
tmddms927 <tmddms927@gmail.com>
2021-05-21 20:19:19 -0700

    feat: add 2588.c

---
+++ easy/2588.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", a*(b%10));
+    printf("%d\n", a*((b/10)%10));
+    printf("%d\n", a*(b/100));
+    printf("%d\n", a*(b % 10) + (a * ((b / 10) % 10))*10 + (a * (b / 100))*100);
+    return 0;
+}
