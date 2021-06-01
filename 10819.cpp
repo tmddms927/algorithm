93d5f34b3e4d5ab63949bbc7aebbf6f79bc80c75
tmddms927 <tmddms927@naver.com>
2021-06-01 15:48:43 +0900

    feat: complete!

--- 10819.cpp
+++ 10819.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 vector<int>v; //받은 정수가 들어있는 배열
 vector<int>t; //조합을 만들 배열
+int N;
+int answer;
 
 int cal()
 {
+	int ret = 0;
 
+	for (int i = 0; i < N-1; ++i)
+		ret += abs(t[i] - t[i + 1]);
+	return (ret);
 }
 
-int set_v(int idx)
+void set_v(int idx)
 {
-	
+	for (int i = 0; i < N; ++i)
+	{
+		if (t[i] == -110)
+		{
+			t[i] = v[idx];
+			if (idx == N-1)
+				answer = max(cal(), answer);
+			else
+				set_v(idx+1);
+			t[i] = -110;
+		}
+	}
 }
 
 int main()
 {
-	int ans = 0, N;
+	answer = 0;
 	cin >> N;
 	t.assign(N, -110); //t -110로 초기화
 	v.assign(N, -110);
 	for (int i = 0; i < N; ++i)
 		cin >> v[i];
-	// set_v(v, t, 0);
-	cout << ans << endl;
+	set_v(0);
+	cout << answer << endl;
 	return 0;
 }
