5aef7925301c706550e0aa1bed0ee021b503029e
tmddms927 <tmddms927@gmail.com>
2021-06-06 20:19:19 -0700

    feat: add 9498.c

---
+++ easy/9498.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    if (a >= 90)
+        printf("A\n");
+    else if (a >= 80)
+        printf("B\n");
+    else if (a >= 70)
+        printf("C\n");
+    else if (a >= 60)
+        printf("D\n");
+    else
+        printf("F\n");
+ 
+    return 0;
+}
