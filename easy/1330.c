2404e3591aff65d9212be5f45f2dac91e0f595b2
tmddms927 <tmddms927@gmail.com>
2021-06-07 20:19:19 -0700

    feat: add 1330.c

---
+++ easy/1330.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ 
+
+int main(void)
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a > b)
+        printf(">\n");
+    else if (a < b)
+        printf("<\n");
+    else
+        printf("==\n");
+    return 0;
+}
