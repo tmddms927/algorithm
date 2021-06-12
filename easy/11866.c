1624b8dec720c6b9afb2c33845eca7a6ad259df5
tmddms927 <tmddms927@gmail.com>
2021-06-11 20:19:19 -0700

    feat: fix algorithm 11866.c fix struct

--- easy/11866.c
+++ easy/11866.c
@@ -1,30 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-//큐 구조체
-typedef char element;
-typedef struct {
-    element queue[1000];
-    int front, rear, num;
-}QueueType;
- 
-void deQueue(QueueType * q, int n)
-{
-    if (q->queue[q->front - 1] == 0)
-    {
-        q->front = (q->front + 3) % n;
-    }
-    else
-    {
-        printf("%d", q->queue[q->front - 1])
-        q->num++;
-        q->queue[q->front - 1] = 0;
-        q->front = (q->front + 3) % n;
-        if (q->num != n - 1)
-            printf(", ");
-    }
-}
- 
 int main(void)
 {
     int n
