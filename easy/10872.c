f225078477a9fcc1e24153f2073be9153dd845cf
tmddms927 <tmddms927@gmail.com>
2021-06-15 20:19:19 -0700

    feat: fix algorithm 10872.c

--- easy/10872.c
+++ easy/10872.c
@@ -1,13 +1,31 @@
+// #include <stdio.h>
+ 
+// int factorial(int a, int n)
+// {
+//     int result = 1;
+//     for (int i = a; i <= n; i++)
+//     {
+//         result = result * i;
+//     }
+//     return result;
+// }
+ 
+// int main(void)
+// {
+//     int n;
+//     scanf("%d", &n);
+ 
+//     printf("%d", factorial(1, n));
+//     return 0;
+// }
+
 #include <stdio.h>
  
 int factorial(int a, int n)
 {
-    int result = 1;
-    for (int i = a; i <= n; i++)
-    {
-        result = result * i;
-    }
-    return result;
+    if (n == a)
+        return a;
+    return n * factorial(a, n-1);
 }
  
 int main(void)
