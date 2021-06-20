77a855c6bf6ba07b5f424f16c27d9d738922df45
tmddms927 <tmddms927@gmail.com>
2021-06-19 20:19:19 -0700

    feat: add algorithm 2869.c

---
+++ easy/2869.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main(main)
+{
+    int a, b, v, height = 0, day = 0;
+    scanf("%d %d %d", &a, &b, &v);
+    day = (v - b) / (a - b);
+    if ((v - b) % (a - b) > 0)
+        day++;
+    printf("%d", day);
+    return 0;
+}
+int main(main)
+{
+    int a, b, v, height = 0, day = 0;
+    scanf("%d %d %d", &a, &b, &v);
+    day = (v - b) / (a - b);
+    if ((v - b) % (a - b) > 0)
+        day++;
+    printf("%d", day);
+    return 0;
+}
--- easy/4673.c
+++ easy/4673.c
@@ -1,58 +1,29 @@
 #include <stdio.h>
-
  
-
 int result(int i)
-
 {
-
     int res = i;
-
     while (i != 0)
-
     {
-
         res += i % 10;
-
         i = i / 10;
-
     }
-
     return res;
-
 }
-
  
-
 int main(void)
-
 {
-
     int a[10000] = {0}, i = 0, x;
-
     for (i = 0; i < 10000; i++)
-
     {
-
         x = result(i);
-
         if (x < 10000)
-
             a[x] = 1;
-
     }
-
     for (i = 0; i < 10000; i++)
-
     {
-
         if (a[i] == 0)
-
             printf("%d\n", i);
-
     }
-
     return 0;
-
 }
-
