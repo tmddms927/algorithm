2015ead82d186c773f717398ce0d7b91025e0c45
seungoh <tmddms927@naver.com>
2021-06-23 11:56:12 +0900

    feat: add 17825

---
+++ bruteforcing/17825.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+vector<int>dice(10);
+vector<int>simul(10, -1);
+pair<int, int> dice_map[4];
+int max_score = 0;
+
+void	dice_set_position(int idx, int i)
+{
+	cout << idx << ", " << dice[i] << ", " << dice_map[idx].first << ", " << dice_map[idx].second << endl;
+	if (dice_map[idx].second == 0)
+		dice_map[idx].first += 2 * dice[i];
+	else if (dice_map[idx].second == 1)
+	{
+		int j;
+		for (j = 0; j < dice[i]; ++j)
+		{
+			dice_map[idx].first += 3;
+			if (dice_map[idx].first > 19)
+				break ;
+		}
+		if (dice_map[idx].first > 19)
+		{
+			dice_map[idx].first = 25;
+			dice_map[idx].second = 4;
+		}
+		if (j < dice[i] - 1)
+			dice_map[idx].first += (dice[i] - j) * 5;
+	}
+	else if (dice_map[idx].second == 2)
+	{
+		int j;
+		for (j = 0; j < dice[i]; ++j)
+		{
+			dice_map[idx].first += 2;
+			if (dice_map[idx].first > 24)
+				break ;
+		}
+		if (dice_map[idx].first > 24)
+		{
+			dice_map[idx].first = 25;
+			dice_map[idx].second = 4;
+		}
+		if (j < dice[i] - 1)
+			dice_map[idx].first += (dice[i] - j) * 5;
+	}
+	else if (dice_map[idx].second == 3)
+	{
+		int j;
+		for (j = 0; j < dice[i]; ++j)
+		{
+			dice_map[idx].first -= 1;
+			if (dice_map[idx].first == 25)
+				break ;
+		}
+		if (dice_map[idx].first  == 25)
+			dice_map[idx].second = 4;
+		if (j < dice[i] - 1)
+			dice_map[idx].first -= (dice[i] - j);
+	}
+	if (dice_map[idx].second == 0)
+	{
+		if (dice_map[idx].first == 10)
+			dice_map[idx].second = 1;
+		else if (dice_map[idx].first == 20)
+			dice_map[idx].second = 2;
+		else if (dice_map[idx].first == 30)
+			dice_map[idx].second = 3;
+	}
+	cout << idx << ", " << dice[i] << ", " << dice_map[idx].first << ", " << dice_map[idx].second << endl;
+}
+
+void	cal()
+{
+	int score = 0;
+
+	for (int i = 0; i < 10; ++i)
+	{
+		dice_set_position(simul[i], i);
+		for (int j = 0; j < 4; ++j)
+		{
+			if (simul[i] == j)
+				continue ;
+			if (dice_map[simul[i]].first == dice_map[j].first && dice_map[simul[i]].second == dice_map[j].second)
+				return ;
+		}
+	}
+	for (int i = 0; i < 4; ++i)
+	{
+		if (dice_map[i].first > 40)
+			continue ;
+		score += dice_map[i].first;
+	}
+	if (score > max_score)
+		max_score = score;
+	exit(0);
+}
+
+void	set_dice(int idx)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		simul[idx] = i;
+		if (idx == 9)
+		{
+			cal();
+			for (int i = 0; i < 4; i++)
+				dice_map[i].first = 0, dice_map[i].second = 0;
+		}
+		else
+			set_dice(idx + 1);
+	}
+}
+
+int		main()
+{
+	for (int i = 0; i < 4; i++)
+		dice_map[i].first = 0, dice_map[i].second = 0;
+	for (int i = 0; i < 10; ++i)
+		cin >> dice[i];
+    set_dice(0);
+	cout << max_score << endl;
+}
