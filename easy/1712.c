a8bff89a33e16cd05daafa1a4b62f37f1c3a2350
tmddms927 <tmddms927@gmail.com>
2021-06-22 20:19:19 -0700

    feat: add algorithm 1712.c

---
+++ easy/1712.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+    int num, i, a, b;
+    scanf("%d", &num);
+    int *n;
+    n = (int*)malloc(sizeof(int)*(num));
+    for (i = 0; i < num; i++)
+    {
+        scanf("%d %d", &a, &b);
+        n[i] = a + b;
+    }
+    for (i = 0; i < num; i++)
+    {
+        printf("%d\n", n[i]);
+    }
+    return 0;
+}
